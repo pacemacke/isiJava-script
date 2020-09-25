839acf31c7cedf4bd1aa05634d22a06e18e42126
peacemaker <70804411+pacemacke@users.noreply.github.com>
2020-09-25 23:41:04 +0400

    Add files via upload

---
+++ массивчки.js
@@ -0,0 +1,40 @@
+var arr = [25,56,51,1,52,2,35,79,85,9885,3];
+
+//for (let k = 0; k < arr.length;k=k+2) {}
+    [arr[0], arr[1] = arr[1], arr[0]]
+    [arr[1], arr[2] = arr[2], arr[1]];
+    
+    console.log(arr);
+/*попытка 
+arr.sort(function wth(a,b,c) {
+    return a-b
+});
+console.log(arr);
+*/
+/*по возрастанию
+arr.sort(function(a, b){return a - b});
+console.log(arr);
+
+по убываню
+arr.sort(function(a, b){return b - a});
+console.log(arr);
+
+*/
+/*var l1,long=arr.length/2;
+for (k=0;k<long;k++){
+l1=arr[k];
+arr[k]=arr[long-k];
+arr[long-k]=l1;
+console.log(arr);
+};
+const even = long => !(long % 2);
+if ((long % 2) =0){
+    for (k=0;k<long;k++){
+        l1=arr[k];
+        arr[k]=arr[long-k];
+        arr[long-k]=l1;
+        };
+        console.log(arr);  
+}
+*/
+//console.log(arr);
